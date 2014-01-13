8e7f26c501052c24a0f2e5be7d919221efab7c07
Glenn <dobestpossible@gmail.com>
2014-01-12 21:51:14 -0700

    Finish demo app

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,8 +11,8 @@ DemoApp::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
+  # Log error messages when you accidentally call methods on nil  DEPRECIATED AND NO LONGER WORKS!!!S
+  #config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -34,4 +34,6 @@ DemoApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  config.eager_load = false
 end
